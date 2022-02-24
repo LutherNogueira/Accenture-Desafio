23de7aca225db38d5645426e7a3d623a617b5623
Andressa Veloso Gonelli Ribeiro <andressa.gonelli@outlook.com>
2022-02-24 10:41:50 -0300

    Alteração do menu: adição de informação sobre header de arquivo de importação

--- src/core/__main__.py
+++ src/core/__main__.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
         print("5 - Sumarizar dados salvos no Banco de dados")
         print("6 - Apagar todos os dados do Banco de dados")
 
-        print("7 - Sair")
+        print("7 - Imprimir na tela modelo de importação")
+        print("8 - Sair")
         print("--------------------------------------------------")
 
         opcao = int(input())
@@ -33,6 +34,14 @@ if __name__ == "__main__":
             case 6:  # Apagar todos os dados do Banco de dados
                 ServiceODBC.deleteAllTables()
             case 7:
+                print("Header de importação do cliente:id;nome;email;data_cadastro;telefone ")
+                print(" id: int \n nome: str\n email: str \n data_cadastro: datetimeoffset \n telefone str")
+                print("Header de importação do transacao:id;cliente_id;valor;data \n")
+                print(" id: int \n cliente_id: int \n valor: float \n data: datetimeoffset\n")
+                
+                print("Atenção:\n- O formato deve ser csv separado por ponto e vírgula. \n- O header deve constar somente no primeiro arquivo \n")
+                print("Nome do arquivo: (O xxx deve ser substituido pelo número do arquivo. Iniciando obrigatóriamente em 001)\n - Cliente: clients-xxx.csv\n - Transacao IN: transaction-in-xxx.csv\n - Transacao IN: transaction-out-xxx.csv")
+            case 8:
                 break
             case _:
                 print("\nOpção inválida\n")
